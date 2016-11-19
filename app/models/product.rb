09ee4c13edb710c4bcb49a0d1af8eddfcda2857c
link-coala <camposcoala@gmail.com>
2016-11-18 20:47:09 -0500

    product_validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,8 @@
 class Product < ActiveRecord::Base
-
+  #activerecord relationship
   has_many :line_items
   has_many :orders, :through => :line_items
+  #validation 
+  validates :name, presence: true,
+                    length: { minimum:  3}
 end
