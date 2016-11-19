c30e3cd28006e1ec92c132e32ce35ef86644b412
link-coala <camposcoala@gmail.com>
2016-11-18 20:26:10 -0500

    form_and_link

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,29 @@
 class ProductsController < ApplicationController
-  def home
   
+  def home
   @product = Product.all
   @order = Order.all
   end
+  
+  def new
+  @product = Product.new
+  end
+  def create
+  @product = Product.new(product_params)
+  if @product.save
+  redirect_to @product
+  else
+    render 'new'
+  end
+end
+  def show
+    @product = Product.find(params[:id])
+  end
 
+
+
+  private
+    def product_params
+      params.require(:product).permit(:name, :price)
+    end
 end
